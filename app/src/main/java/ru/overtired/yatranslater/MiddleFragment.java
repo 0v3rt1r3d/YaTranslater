8d5accdd770beff680ff6177fc277ace3812d1e1
overtired <overtired2014@gmail.com>
2017-03-17 12:48:22 +0000

    Сделал ViewPager, который почему-то не работает как надо(

--- app/src/main/java/ru/overtired/yatranslater/MiddleFragment.java
+++ app/src/main/java/ru/overtired/yatranslater/MiddleFragment.java
@@ -3,10 +3,16 @@ package ru.overtired.yatranslater;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by overtired on 16.03.17.
@@ -14,11 +20,17 @@ import android.widget.TextView;
 
 public class MiddleFragment extends Fragment
 {
+    private static final String TAG = "MiddleFragment: ";
+
     private FavoriteFragment mFavoriteFragment;
     private HistoryFragment mHistoryFragment;
 
+    private ImageButton mClearHistoryButton;
+
     private TextView mHistoryTextView;
     private TextView mFavoriteTextView;
+
+    private ViewPager mViewPager;
 
     public static MiddleFragment newInstance()
     {
@@ -37,9 +49,27 @@ public class MiddleFragment extends Fragment
         mFavoriteFragment = FavoriteFragment.newInstance();
         mHistoryFragment = HistoryFragment.newInstance();
 
-        getActivity().getSupportFragmentManager().beginTransaction()
-                .add(R.id.middle_fragment_container, mHistoryFragment)
-                .commit();
+        mViewPager = (ViewPager) v.findViewById(R.id.middle_view_pager);
+//        mViewPager.setAdapter(new FragmentPagerAdapter(getFragmentManager())
+//        {
+//            @Override
+//            public Fragment getItem(int position)
+//            {
+//                if(position == 0)
+//                {
+//                    return mHistoryFragment;
+//                } else
+//                {
+//                    return mFavoriteFragment;
+//                }
+//            }
+//
+//            @Override
+//            public int getCount()
+//            {
+//                return 2;
+//            }
+//        });
 
         mFavoriteTextView = (TextView) v.findViewById(R.id.favorites_title);
         mFavoriteTextView.setOnClickListener(new View.OnClickListener()
@@ -47,13 +77,12 @@ public class MiddleFragment extends Fragment
             @Override
             public void onClick(View v)
             {
-                if (getActivity().getSupportFragmentManager()
-                        .findFragmentById(R.id.middle_fragment_container) != mFavoriteFragment)
-                {
-                    getActivity().getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.middle_fragment_container, mFavoriteFragment)
-                            .commit();
-                }
+                Toast.makeText(getActivity(),"Work! favorites",Toast.LENGTH_SHORT).show();
+//                Log.d(TAG,Integer.toString(mViewPager.getCurrentItem()));
+//                if (mViewPager.getCurrentItem()!=1)
+//                {
+//                    mViewPager.setCurrentItem(0);
+//                }
             }
         });
 
@@ -63,16 +92,24 @@ public class MiddleFragment extends Fragment
             @Override
             public void onClick(View v)
             {
-                if (getActivity().getSupportFragmentManager()
-                        .findFragmentById(R.id.middle_fragment_container) != mHistoryFragment)
-                {
-                    getActivity().getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.middle_fragment_container, mHistoryFragment)
-                            .commit();
-                }
+                Toast.makeText(getActivity(),"Work! favorites",Toast.LENGTH_SHORT).show();
+//                Log.d(TAG,Integer.toString(mViewPager.getCurrentItem()));
+//                if (mViewPager.getCurrentItem()!=0)
+//                {
+//                    mViewPager.setCurrentItem(1);
+//                }
             }
         });
 
+        mClearHistoryButton = (ImageButton) v.findViewById(R.id.clear_history_button);
+        mClearHistoryButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getActivity(),"WORK!",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return v;
     }
